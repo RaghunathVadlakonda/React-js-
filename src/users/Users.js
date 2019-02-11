c16739849a0a887ef91f4aafe0ea1fd298811cc1
Raghunath Vadlakonda <raghu@Raghunaths-MacBook-Air.local>
2019-02-11 12:50:13 +0530

    Adding new Component Users and importing in App file

---
+++ src/users/Users.js
@@ -0,0 +1,12 @@
+// Users Component
+import React, { Component} from 'react';
+
+class Users extends Component {
+    render() {
+        return (
+            <div> Hi!, Welcome to Second Component </div>
+        );
+    }
+}
+
+export default Users;
